8f383c61c578a675076e4efdf1e4f95c1ee70d01
Chris Lopez <christian.lopez.be@gmail.com>
2022-09-01 20:14:42 -0500

    Add vuetify,eslint and vuetify components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-text-field outlined single-line label="Añadir" type="text">
+                    <template v-slot:append>
+                        <v-btn tile color="blue white--text" @click="addVideo"
+                            >Añadir</v-btn
+                        >
+                    </template>
+                </v-text-field>
+            </v-col>
+        </v-row>
+        <v-divider />
+        <v-row>
+            <v-col cols="12">
+                <TheYoutubeVideoList />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from 'vue';
 
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
+import TheYoutubeVideoList from '@/components/TheYoutubeVideoList';
+
+export default defineComponent({
+    name: 'HomeView',
+    components: {
+        TheYoutubeVideoList,
+    },
+    methods: {
+        addVideo() {
+            console.log('Add a video!');
+        },
+    },
+});
 </script>
